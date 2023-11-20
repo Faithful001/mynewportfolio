1926281212bd5297eb97ae0fb279191686f6a9de
Faithful <faithfuleromosele001@gmail.com>
2023-11-20 23:53:55 +0100

    let's see

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import emoji from "../assets/memoji1.png";
+import my_resume from "./my_latest_fullstack_resume.pdf";
 
 const Home = () => {
 	return (
@@ -23,7 +24,7 @@ const Home = () => {
 					</p>
 					<div className="mt-5">
 						<a
-							href="my_latest_fullstack_resume.pdf"
+							href={my_resume}
 							download
 							className="flex items-center justify-center w-28 transition-all duration-500 bg-[#000000] outline-none outline-offset-1 outline-[#1B99F7] py-2 px-7 rounded hover:bg-[#1b98f734] text-[#1B99F7] text-sm"
 						>
