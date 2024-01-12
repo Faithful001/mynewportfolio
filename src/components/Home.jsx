ccec85f2e8e74213e55a29f7aff1d12cd94172c7
Faithful <faithfuleromosele001@gmail.com>
2024-01-12 02:49:08 +0100

    changed resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import emoji from "../assets/memoji1.png";
-import my_resume from "./my_latest_fullstack_resume.pdf";
+import my_resume from "./fullstack_resume.pdf";
 
 const Home = () => {
 	return (
