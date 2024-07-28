4406e3165f162f7c6c77c579d462a18e3ea5c88e
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2024-07-28 16:24:30 +0100

    added a new resume and new projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,45 +3,47 @@ import emoji from "../assets/memoji1.png";
 import my_resume from "./fullstack_resume.pdf";
 
 const Home = () => {
-	return (
-		<div
-			className="home relative md:bottom-20 bottom-[20px] md:px-10 px-7 md:h-screen flex flex-col md:flex-row items-center justify-center"
-			id="home"
-		>
-			{/* <div className="section "> */}
-			<div className="section flex flex-col md:flex-row items-center justify-between w-full">
-				{/* image & content */}
-				{/* text */}
-				<div className="text-white">
-					<h6 className="text-xl mt-12 -mb-2 opacity-80">Hello, my name is</h6>
-					<h1 className="md:leading-[70px] leading-[50px] my-4 md:text-[64px] text-[50px] text-[#1B99F7] font-bold">
-						Faithful Eromosele
-					</h1>
-					<p className="max-w-xl opacity-80">
-						Software Developer & Problem Solver. I build deliberate world-class
-						software. My job is to give you a beautiful, user-friendly, yet
-						sophiticated product
-					</p>
-					<div className="mt-5">
-						<a
-							href={my_resume}
-							download
-							className="flex items-center justify-center w-28 transition-all duration-500 bg-[#000000] outline-none outline-offset-1 outline-[#1B99F7] py-2 px-7 rounded hover:bg-[#1b98f734] text-[#1B99F7] text-sm"
-						>
-							Resume
-							<span className="material-symbols-outlined">download</span>
-						</a>
-					</div>
-				</div>
-			</div>
-			<img
-				className="md:h-96 md:max-w-[384px] h-72 max-w-[288px]"
-				src={emoji}
-				alt=""
-			/>
-			{/* </div> */}
-		</div>
-	);
+  return (
+    <div
+      className="home relative md:bottom-20 bottom-[20px] md:px-10 px-7 md:h-screen flex flex-col md:flex-row items-center justify-center"
+      id="home"
+    >
+      {/* <div className="section "> */}
+      <div className="flex flex-col items-center justify-between w-full section md:flex-row">
+        {/* image & content */}
+        {/* text */}
+        <div className="text-white">
+          <h6 className="mt-12 -mb-2 text-xl opacity-80">Hello, my name is</h6>
+          <h1 className="md:leading-[70px] leading-[50px] my-4 md:text-[64px] text-[50px] text-[#1B99F7] font-bold">
+            Faithful Eromosele
+          </h1>
+          <p className="max-w-xl opacity-80">
+            Software Developer & Problem Solver. I build deliberate world-class
+            software. My job is to give you a beautiful, user-friendly, yet
+            sophiticated product
+          </p>
+          <div className="mt-5">
+            <a
+              href="https://drive.google.com/file/d/17rJ44EqTe3WWsWHchCqFzaQRUVi7pPHd/view?usp=sharing"
+              //   download
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center w-28 transition-all duration-500 bg-[#000000] outline-none outline-offset-1 outline-[#1B99F7] py-2 px-7 rounded hover:bg-[#1b98f734] text-[#1B99F7] text-sm"
+            >
+              Resume
+              <span className="material-symbols-outlined">download</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <img
+        className="md:h-96 md:max-w-[384px] h-72 max-w-[288px]"
+        src={emoji}
+        alt=""
+      />
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,168 +7,189 @@ import saler from "../assets/projects/saler.png";
 import vulue from "../assets/projects/vulue.png";
 import mealstaer from "../assets/projects/mealstaer.png";
 import bandage from "../assets/projects/bandage.png";
+import owe_app from "../assets/projects/owe-app.png";
 import { useState } from "react";
 
 const Projects = () => {
-	const [showPrivate, setShowPrivate] = useState("");
-	// console.log(showPrivate);
+  const [showPrivate, setShowPrivate] = useState("");
+  // console.log(showPrivate);
 
-	function handleMouseEnter(project) {
-		setShowPrivate(`true ${project}`);
-	}
-	function handleMouseLeave(project) {
-		setShowPrivate(`false ${project}`);
-	}
+  function handleMouseEnter(project) {
+    setShowPrivate(`true ${project}`);
+  }
+  function handleMouseLeave(project) {
+    setShowPrivate(`false ${project}`);
+  }
 
-	function handleClick(e) {
-		e.preventDefault();
-		// Handle the click event logic here, if needed
-	}
-	const projects = [
-		{
-			title: "FINANCE TRACKER",
-			description:
-				"A fullstack webapp that helps you keep record of your transactions",
-			technologies: ["React JS", "Node JS", "REST API"],
-			live_url: "https://finance-tracker-app1.netlify.app",
-			github_url: "https://github.com/Faithful001/finance-tracker-app",
-			photo: financetracker,
-		},
-		{
-			title: "MEALSTAER",
-			description: "Your meal planning buddy. Organize your meals on the go.",
-			technologies: ["React JS", "Node JS", "REST API"],
-			live_url: "https://mealstaer.vercel.app",
-			github_url: "https://github.com/Faithful001/mealstaer",
-			photo: mealstaer,
-		},
-		{
-			title: "BANDAGE",
-			description: "Landing page project with add-to-cart functionality.",
-			technologies: ["Next JS", "Tailwind"],
-			live_url: "https://basket-ecommerce.vercel.app",
-			github_url: "",
-			photo: bandage,
-		},
-		{
-			title: "SALER APP LANDING PAGE",
-			description: "Landing page of the saler app.",
-			technologies: ["React JS", "Tailwind"],
-			live_url: "https://saler-app.vercel.app/",
-			github_url: "https://github.com/Faithful001/saler-landing_page",
-			photo: saler,
-		},
-		{
-			title: "WORKOUT BUDDY",
-			description:
-				"A fullstack webapp that helps you keep record of your workout exercises",
-			technologies: ["React JS", "Node JS", "REST API"],
-			live_url: "https://work-o-buddy.vercel.app/",
-			github_url: "https://github.com/Faithful001/workout-app",
-			photo: workoutbuddy,
-		},
-		{
-			title: "VULUE LANDING PAGE",
-			description: "Landing page of the vulue app.",
-			technologies: ["React JS", "Tailwind"],
-			live_url: "https://vulue.org/",
-			github_url: "https://github.com/Faithful001/vulue_landing_page",
-			photo: vulue,
-		},
-		{
-			title: "DIKTION",
-			description:
-				"A dictionary app to search for your favourite words. Dare to search!",
-			technologies: ["React JS", "Typescript", "REST API"],
-			live_url: "https://diktion.vercel.app/",
-			github_url: "https://github.com/Faithful001/diktion",
-			photo: diktion,
-		},
-	];
+  function handleClick(e) {
+    e.preventDefault();
+    // Handle the click event logic here, if needed
+  }
+  const projects = [
+    {
+      title: "FINANCE TRACKER",
+      description:
+        "Your Comprehensive Full-Stack Web Application for Seamless Transaction Management: A dynamic full-stack web application designed to effortlessly record and manage your financial transactions",
+      technologies: ["React JS", "Node JS", "REST API"],
+      live_url: "https://finance-tracker-app1.netlify.app",
+      github_url: "https://github.com/Faithful001/finance-tracker-app",
+      photo: financetracker,
+    },
+    {
+      title: "MEALSTAER",
+      description:
+        "Your meal planning buddy. Effortlessly organize and plan your meals anytime, anywhere.",
+      technologies: ["React JS", "Node JS", "REST API"],
+      live_url: "https://mealstaer.vercel.app",
+      github_url: "https://github.com/Faithful001/mealstaer",
+      photo: mealstaer,
+    },
+    {
+      title: "OWE APP",
+      description:
+        "Owe is your Yoruba learning assistant. Level up your Yoruba, with every proverb. Yoruba wisdom, at your fingertips!",
+      technologies: [
+        "Next JS",
+        "Gemini API",
+        "Text-to-Speech",
+        "Speech-to-Text",
+      ],
+      live_url: "https://owe-web.vercel.app/",
+      github_url: "",
+      photo: owe_app,
+    },
+    {
+      title: "SALER APP LANDING PAGE",
+      description:
+        "Saler App Landing Page: Captivating entry point designed to engage and inform users about the Saler app.",
+      technologies: ["React JS", "Tailwind"],
+      live_url: "https://saler-app.vercel.app/",
+      github_url: "https://github.com/Faithful001/saler-landing_page",
+      photo: saler,
+    },
+    {
+      title: "WORKOUT BUDDY",
+      description:
+        "A comprehensive fullstack web app designed to help you effortlessly log and monitor your workout exercises. Stay motivated with detailed exercise records and performance analytics.",
+      technologies: ["React JS", "Node JS", "REST API"],
+      live_url: "https://work-o-buddy.vercel.app/",
+      github_url: "https://github.com/Faithful001/workout-app",
+      photo: workoutbuddy,
+    },
 
-	// const techs = projects.map((tech) => {
-	// 	return tech.technologies.split(",");
-	// });
+    // {
+    // 	title: "VULUE LANDING PAGE",
+    // 	description: "Landing page of the vulue app.",
+    // 	technologies: ["React JS", "Tailwind"],
+    // 	live_url: "https://vulue.org/",
+    // 	github_url: "https://github.com/Faithful001/vulue_landing_page",
+    // 	photo: vulue,
+    // },
+    {
+      title: "DIKTION",
+      description:
+        "An intuitive app for searching your favorite words. Discover definitions, synonyms, and more. Dare to search and expand your vocabulary!",
+      technologies: ["React JS", "Typescript", "REST API"],
+      live_url: "https://diktion.vercel.app/",
+      github_url: "https://github.com/Faithful001/diktion",
+      photo: diktion,
+    },
+    {
+      title: "BANDAGE LANDING PAGE",
+      description:
+        "A sleek, responsive landing page featuring an intuitive add-to-cart functionality. Enhance your shopping experience with seamless product selection and purchase capabilities.",
+      technologies: ["Next JS", "Tailwind"],
+      live_url: "https://basket-ecommerce.vercel.app",
+      github_url: "",
+      photo: bandage,
+    },
+  ];
 
-	return (
-		// changeable
-		<div
-			className="md:px-10 px-7 my-5 pt-5 flex flex-col items-center"
-			id="projects"
-		>
-			<h1 className="text-[#1B99F7] font-semibold text-2xl mt-16">
-				Featured Projects<span className={`animate-bounce`}>👨‍🍳</span>
-			</h1>
-			<p className="my-3 text-white text-center text-sm md:w-3/4 leading-[27px]">
-				I have worked on many projects over the course of being a software
-				developer, here are a few of my live, real-world projects
-			</p>
-			{/* featured projects */}
+  // const techs = projects.map((tech) => {
+  // 	return tech.technologies.split(",");
+  // });
 
-			{/* changeable */}
-			<div className="wrapper mt-8 flex flex-col items-center max-w-3xl">
-				{projects.map((project, index) => (
-					<span className="md:flex mb-12 gap-5 max-w-5xl" key={index}>
-						<div className="mb-4 flex justify-center">
-							<img
-								className="project-image opacity-40 hover:opacity-80 transition-all duration-500 max-w-[480px] w-full rounded-lg"
-								src={project.photo}
-								alt=""
-							/>
-						</div>
-						<div className="relative flex flex-col justify-center items-start">
-							<div className="gap-2 flex items-center mb-3 px-2">
-								<p className="font-medium text-xl">{project.title}</p>
-								<div
-									className={`private absolute flex items-center justify-between md:-top-2 -top-7 left-0 text-xs text-white opacity-0 transition-opacity duration-300 bg-[#3a3a3abd] p-1 px-2 rounded-lg ${
-										showPrivate === `true ${project.title}`
-											? "opacity-100"
-											: "opacity-0"
-									}`}
-								>
-									<span className="material-symbols-outlined text-xs mr-1">
-										lock
-									</span>
-									<p>This repository is private</p>
-								</div>
-								<a
-									href={project.github_url}
-									onClick={(e) => project.github_url == "" && handleClick(e)}
-									onMouseEnter={() =>
-										project.github_url == "" && handleMouseEnter(project.title)
-									}
-									onMouseLeave={() =>
-										project.github_url == "" && handleMouseLeave(project.title)
-									}
-								>
-									<img className="h-6" src={github} alt="" />
-								</a>
-							</div>
+  return (
+    // changeable
+    <div
+      className="flex flex-col items-center pt-5 my-5 md:px-10 px-7"
+      id="projects"
+    >
+      <h1 className="text-[#1B99F7] font-semibold text-2xl mt-16">
+        Featured Projects<span className={`animate-bounce`}>👨‍🍳</span>
+      </h1>
+      <p className="my-3 text-white text-center text-sm md:w-3/4 leading-[27px]">
+        I have worked on many projects over the course of being a software
+        developer, here are a few of my live, real-world projects
+      </p>
+      {/* featured projects */}
 
-							<div className="gap-3 flex mb-3">
-								{project.technologies.map((tech, index) => (
-									<div key={index} className="">
-										<p className="rounded-md bg-[#1B99F7] p-1 px-2 text-[11px]">
-											{tech}
-										</p>
-									</div>
-								))}
-							</div>
-							<p className="mb-2 font-extralight text-[14px]">
-								{project.description}
-							</p>
-							<a
-								href={project.live_url}
-								className="rounded-lg p-2 text-xs bg-[#1a1a1a] border-[1px] border-white text-white duration-300 hover:bg-[#1B99F7] hover:text-white"
-							>
-								View Project
-							</a>
-						</div>
-					</span>
-				))}
-			</div>
-		</div>
-	);
+      {/* changeable */}
+      <div className="flex flex-col items-center max-w-3xl mt-8 wrapper">
+        {projects.map((project, index) => (
+          <span className="max-w-5xl gap-5 mb-12 md:flex" key={index}>
+            <div className="flex justify-center mb-4">
+              <img
+                className="project-image opacity-40 hover:opacity-80 transition-all duration-500 max-w-[480px] w-full rounded-lg"
+                src={project.photo}
+                alt=""
+              />
+            </div>
+            <div className="relative flex flex-col items-start justify-center w-full md:w-1/2">
+              <div className="flex items-center gap-2 px-2 mb-3">
+                <p className="text-xl font-medium">{project.title}</p>
+                <div
+                  className={`private absolute flex items-center justify-between md:-top-2 -top-7 left-0 text-xs text-white opacity-0 transition-opacity duration-300 bg-[#3a3a3abd] p-1 px-2 rounded-lg ${
+                    showPrivate === `true ${project.title}`
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                >
+                  <span className="mr-1 text-xs material-symbols-outlined">
+                    lock
+                  </span>
+                  <p>This repository is private</p>
+                </div>
+                <a
+                  href={project.github_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => project.github_url == "" && handleClick(e)}
+                  onMouseEnter={() =>
+                    project.github_url == "" && handleMouseEnter(project.title)
+                  }
+                  onMouseLeave={() =>
+                    project.github_url == "" && handleMouseLeave(project.title)
+                  }
+                >
+                  <img className="h-6" src={github} alt="" />
+                </a>
+              </div>
+
+              <div className="flex gap-3 mb-3">
+                {project.technologies.map((tech, index) => (
+                  <div key={index} className="">
+                    <p className="rounded-md bg-[#1B99F7] p-1 px-2 text-[11px]">
+                      {tech}
+                    </p>
+                  </div>
+                ))}
+              </div>
+              <p className="mb-2 font-extralight text-[14px]">
+                {project.description}
+              </p>
+              <a
+                href={project.live_url}
+                className="rounded-lg p-2 text-xs bg-[#1a1a1a] border-[1px] border-white text-white duration-300 hover:bg-[#1B99F7] hover:text-white"
+              >
+                View Project
+              </a>
+            </div>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
