bc25e1bb2ba9326846d3142764a16bc061729a88
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2025-04-14 22:34:46 +0100

    refactored the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,11 +28,13 @@ const Header = () => {
 
   return (
     <div
-      className={
-        navbar
-          ? "header active w-[90%] fixed bg-[#1b1b1b] backdrop-blur-md rounded-full z-10 bg-opacity-30 border-2 border-opacity-40 border-[#3a3a3a]"
-          : "header w-full mt-2 z-50"
-      }
+      className={`
+        header
+        ${
+          navbar
+            ? "active w-[90%] fixed bg-[#1b1b1b] backdrop-blur-md bg-opacity-30 border-2 border-opacity-40 border-[#3a3a3a] rounded-full z-10 "
+            : "w-full mt-2 z-50"
+        }`}
     >
       <div className="z-50 items-center justify-between py-4 md:flex md:px-10 px-7">
         {/* logo section */}
@@ -63,9 +65,11 @@ const Header = () => {
         </div>
         {/* link items | add sm:bg-transparent below if there is any transparency problem*/}
         <ul
-          className={`md:flex md:items-center md:pb-0 absolute md:static z-10 left-0 w-full md:w-auto mt-6 md:mt-0 md:pl-0 pl-9 md:bg-transparent bg-[#1b1b1b] rounded-3xl opacity-[0.98] transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 absolute md:static z-10 left-0 w-full md:w-auto mt-6 md:mt-0 md:pl-0 pl-9 md:bg-transparent md:bg-opacity-100 bg-opacity-95 bg-[#1b1b1b] rounded-3xl transition-all duration-500 ease-in ${
             open ? "top-12" : "top-[-490px]"
-          }`}
+            // opacity-[0.98]
+          }
+          `}
         >
           {Links.map((link) => (
             <li
