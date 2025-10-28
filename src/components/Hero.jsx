dd9d05b90b3a2b928f5009ba7b2c21d864393c15
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2025-10-28 10:01:21 +0100

    updated my resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
           <div className="mt-5">
             <a
-              href="https://drive.google.com/file/d/1VpgEhGiA6MER2XU4tD6hZrSZ4c-YCM76/view?usp=sharing"
+              href="https://drive.google.com/file/d/13wYW3pImboRYVlWLFDujI8HjHSu6WYm2/view?usp=sharing"
               //   download
               target="_blank"
               rel="noopener noreferrer"
